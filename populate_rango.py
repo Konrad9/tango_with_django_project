9347de89c57c6ef512016a105cfe3967954fd2b4
Konrad Grudzinski <2408448g@student.gla.ac.uk>
2020-02-02 22:53:35 +0000

    chapter 6 finished, tests OK

--- populate_rango.py
+++ populate_rango.py
@@ -15,39 +15,47 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
+         'url':'http://docs.python.org/3/tutorial/',
+         "views": 11},
         {'title':'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/'},
+         'url':'http://www.greenteapress.com/thinkpython/',
+         "views": 22},
         {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'} ]
+         'url':'http://www.korokithakis.net/tutorials/python/',
+         "views": 33} ]
     django_pages = [
         {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         "views": 44},
         {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
+         'url':'http://www.djangorocks.com/',
+         "views": 55},
         {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'} ]
+         'url':'http://www.tangowithdjango.com/',
+         "views": 66} ]
 
     other_pages = [
     {'title':'Bottle',
-    'url':'http://bottlepy.org/docs/dev/'},
+    'url':'http://bottlepy.org/docs/dev/',
+    "views": 69},
     {'title':'Flask',
-    'url':'http://flask.pocoo.org'} ]
-
+    'url':'http://flask.pocoo.org',
+    "views": 77} ]
+    
     cats = {'Python': {'pages': python_pages, "views": 128, "likes": 64},
     'Django': {'pages': django_pages, "views": 64, "likes": 32},
     'Other Frameworks': {'pages': other_pages, "views": 32, "likes": 16} }
-
+    
     # If you want to add more categories or pages,
     # add them to the dictionaries above.
-
+    
     # The code below goes through the cats dictionary, then adds each category,
     # and then adds all the associated pages for that category.
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
-
+            add_page(c, p['title'], p['url'], p["views"])
+    
     # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -71,3 +79,4 @@ def add_cat(name, views=0, likes=0):
 if __name__ == '__main__':
     print('Starting Rango population script...')
     populate()
+    
--- rango/views.py
+++ rango/views.py
@@ -1,18 +1,61 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
-#def index(request):
- #   return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")
 
+# responsible for main page view
 def index(request):
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only -- or all if less than 5.
+    category_list = Category.objects.order_by("-likes")[:5]
+    pages = Page.objects.order_by("-views")[:5]
+    
     # Construct a dictionary to pass to the template engine as its context.
+    # Place the list in our context_dict dictionary (with our boldmessage!)
+    # that will be passed to the template engine.
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {"boldmessage":"Crunchy, creamy, cookie, candy, cupcake!"}
-
+    context_dict = {}
+    context_dict["boldmessage"] = "Crunchy, creamy, cookie, candy, cupcake!"
+    context_dict["pages"] = pages
+    context_dict["categories"] = category_list
+    
+    
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, "rango/index.html", context=context_dict)
 
+    
 def about(request):
-    return render(request, "rango/about.html", context={"boldmessage":"This tutorial has been put together by Konrad."}) 
+    return render(request, "rango/about.html", context={"boldmessage":"This tutorial has been put together by me lol."})
+
+def show_category(request, category_name_slug):
+    # Create a context dictionary which we can pass
+    # to the template rndering engine
+    context_dict = {}
+    
+    try:
+        # Can we find a category name slug with the given name?
+        # If yes, then the .get() method returns one model instance.
+        # If we cannot, the .get() method raises a DoesNotExist exception.
+        category = Category.objects.get(slug = category_name_slug)
+        
+        # Retrive all of the associated pages.
+        # The filter() will return a list of page objects or an empty list.
+        pages = Page.objects.filter(category = category)#.order_by("-views")[:5]
+        
+        # Adds our results list to the template context under name pages.
+        context_dict["pages"] = pages
+        # We also add the category object from the database to the 
+        # context dictionary. We will use this in the templage to verify
+        # to verify that the category exists.
+        context_dict["category"] = category
+    except Category.DoesNotExist:
+        # We get here if we didn't find the specified category. In this case,
+        # the template will display the "no category" message.
+        context_dict["category"] = None
+        context_dict["pages"] = None
+        
+    # Render the response and return it ot the client
+    return render(request, "rango/category.html", context=context_dict)
